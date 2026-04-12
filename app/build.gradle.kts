4bdd3463180e2e1303818dc98db043c17daba274
EmperorKabir <bhasin.kabir@gmail.com>
2026-04-12 21:46:09 +0100

    Auto-commit before addressing Gradle and Java configuration issues

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,8 +21,14 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
+        sourceCompatibility = JavaVersion.VERSION_21
+        targetCompatibility = JavaVersion.VERSION_21
+    }
+}
+
+java {
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(21))
     }
 }
 
